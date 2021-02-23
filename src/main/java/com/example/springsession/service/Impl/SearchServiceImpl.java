9f334eaef4325878ee623ace4a6ac9481f026adf
Srikanta Vaibhav N <srikantavaibhavn@Srikantas-MacBook-Pro.local>
2021-02-23 13:44:40 +0530

    v3 first commit

--- src/main/java/com/example/springsession/service/Impl/SearchServiceImpl.java
+++ src/main/java/com/example/springsession/service/Impl/SearchServiceImpl.java
@@ -9,9 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SearchServiceImpl implements SearchService {
+
+
 
     @Autowired
     private SearchClient searchClient;
@@ -19,44 +25,87 @@ public class SearchServiceImpl implements SearchService {
     @Override
     public SearchResponseDTO getProducts( ProductRequestDTO request) {
 
-        ProductResponseDTO myProductDetails[] = new ProductResponseDTO[10];
-        ProductResponseDTO myProductDetailsLoc[] = new ProductResponseDTO[10];
-
-        Map<String, Object> productsResponse = searchClient.getProducts(request.getSearchTerm());
-        Map<String,Object> response = (Map<String, Object>)productsResponse.get("response");
-        List<Map<String, Object>> products = (List<Map<String, Object>>)response.get("docs");
-
-        Map<String, Object> productsResponseLoc = searchClient.getProducts("stockLocation"+request.getStockLocation());
-        Map<String,Object> responseLoc = (Map<String, Object>)productsResponse.get("response");
-        List<Map<String, Object>> productsLoc = (List<Map<String, Object>>)response.get("docs");
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
         SearchResponseDTO responseDTO = new SearchResponseDTO();
-        ProductResponseDTO[] product = new ProductResponseDTO[10];
+        ProductResponseDTO myProductDetailsLoc[] = new ProductResponseDTO[10];
+        ProductResponseDTO myProductDetails[] = new ProductResponseDTO[10];
 
-        for(int i=0; i<10; i++) {
-            myProductDetails[i] = new ProductResponseDTO();
-            int stock = (int)products.get(i).get("isInStock");
-            if(stock==1)
-                myProductDetails[i].setInStock(true);
-            else
-                myProductDetails[i].setInStock(false);
-            myProductDetails[i].setTitle((String) products.get(i).get("name"));
-            myProductDetails[i].setSalesPrice((int)((double) products.get(i).get("salePrice")));
-            myProductDetails[i].setDescription((String) products.get(i).get("description"));
+
+        Runnable runnableTask1 = () ->
+        {
+            try {
+                System.out.println(Thread.currentThread().getName());
+                Map<String, Object> productsResponse = searchClient.getProducts(request.getSearchTerm());
+                Map<String, Object> response = (Map<String, Object>) productsResponse.get("response");
+                List<Map<String, Object>> products = (List<Map<String, Object>>) response.get("docs");
+
+                for (int i = 0; i < 10; i++) {
+                    myProductDetails[i] = new ProductResponseDTO();
+                    int stock = (int) products.get(i).get("isInStock");
+                    if (stock == 1)
+                        myProductDetails[i].setInStock(true);
+                    else
+                        myProductDetails[i].setInStock(false);
+                    myProductDetails[i].setTitle((String) products.get(i).get("name"));
+                    myProductDetails[i].setSalesPrice((int) ((double) products.get(i).get("salePrice")));
+                    myProductDetails[i].setDescription((String) products.get(i).get("description"));
+                }
+
+                responseDTO.setProducts(Arrays.asList(myProductDetailsLoc));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        };
+
+
+        Runnable runnableTask2 = () -> {
+            try {
+                System.out.println(Thread.currentThread().getName());
+                Map<String, Object> productsResponseLoc = searchClient.getProducts("stockLocation" + request.getStockLocation());
+                Map<String, Object> responseLoc = (Map<String, Object>) productsResponseLoc.get("response");
+                List<Map<String, Object>> productsLoc = (List<Map<String, Object>>) responseLoc.get("docs");
+
+
+                ProductResponseDTO[] product = new ProductResponseDTO[10];
+
+                for (int i = 0; i < 10; i++) {
+                    myProductDetailsLoc[i] = new ProductResponseDTO();
+                    int stock = (int) productsLoc.get(i).get("isInStock");
+                    if (stock == 1)
+                        myProductDetailsLoc[i].setInStock(true);
+                    else
+                        myProductDetailsLoc[i].setInStock(false);
+                    myProductDetailsLoc[i].setTitle((String) productsLoc.get(i).get("name"));
+                    myProductDetailsLoc[i].setSalesPrice((int) ((double) productsLoc.get(i).get("salePrice")));
+                    myProductDetailsLoc[i].setDescription((String) productsLoc.get(i).get("description"));
+                }
+
+
+                responseDTO.setLocationBaseProducts(Arrays.asList(myProductDetailsLoc));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+
+
+
+
+
+
+        executorService.execute(runnableTask1);
+        executorService.execute(runnableTask2);
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
         }
-        for(int i=0; i<10; i++) {
-            myProductDetailsLoc[i] = new ProductResponseDTO();
-            int stock = (int)productsLoc.get(i).get("isInStock");
-            if(stock==1)
-                myProductDetailsLoc[i].setInStock(true);
-            else
-                myProductDetailsLoc[i].setInStock(false);
-            myProductDetailsLoc[i].setTitle((String) productsLoc.get(i).get("name"));
-            myProductDetailsLoc[i].setSalesPrice((int)((double) productsLoc.get(i).get("salePrice")));
-            myProductDetailsLoc[i].setDescription((String) productsLoc.get(i).get("description"));
-        }
-        responseDTO.setProducts(Arrays.asList(myProductDetails));
-        responseDTO.setLocationBaseProducts(Arrays.asList(myProductDetailsLoc));
+
         return responseDTO;
+
     }
 }
